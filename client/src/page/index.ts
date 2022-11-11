0a31cc848f4dbea092efdfa8a2e5aceff96b591f
ian038 <ianphua3@gmail.com>
2022-11-11 10:31:14 -0500

    Completed app

--- client/src/page/index.ts
+++ client/src/page/index.ts
@@ -2,5 +2,6 @@ import Home from "./Home";
 import CreateBattle from "./CreateBattle";
 import Battle from "./Battle";
 import Battleground from "./Battleground";
+import JoinBattle from "./JoinBattle";
 
-export { Battle, Home, Battleground, CreateBattle };
+export { Battle, Home, Battleground, CreateBattle, JoinBattle };
